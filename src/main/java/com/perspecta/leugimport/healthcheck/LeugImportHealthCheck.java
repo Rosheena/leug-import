6601fc285d5a856ec7c8b267d4a814e3661e394c
Rosheena <rosheena.khan@gmail.com>
2019-07-10 17:04:39 -0400

    Health check

---
+++ src/main/java/com/perspecta/leugimport/healthcheck/LeugImportHealthCheck.java
@@ -0,0 +1,14 @@
+package com.perspecta.leugimport.healthcheck;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LeugImportHealthCheck implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		return Health.up().build();
+	}
+}
