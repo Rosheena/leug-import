57f6eb242cb9ba490f93fe3681d6a44b85970ccf
Rosheena <rosheena.khan@gmail.com>
2019-07-10 17:29:45 -0400

    Spring Boot Application Initializer

--- src/main/java/com/perspecta/leugimport/LeugImportApplication.java
+++ src/main/java/com/perspecta/leugimport/LeugImportApplication.java
@@ -1,7 +1,6 @@
 package com.perspecta.leugimport;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
