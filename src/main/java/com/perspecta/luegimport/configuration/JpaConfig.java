2eac543b585e10a72b12bf7b8250923738701fc3
Rosheena <rosheena.khan@gmail.com>
2019-07-17 12:33:54 -0400

    application.yml changes

--- src/main/java/com/perspecta/luegimport/configuration/JpaConfig.java
+++ src/main/java/com/perspecta/luegimport/configuration/JpaConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableJpaRepositories(basePackages = {"com.perspecta.leugimport.business.domain"})
+@EnableJpaRepositories(basePackages = {"com.perspecta.luegimport.business.domain"})
 @EnableTransactionManagement
 @Configuration
 public class JpaConfig {
