394c9c34b557140cd636204caeb21a029f29cdfc
Rosheena <rosheena.khan@gmail.com>
2019-07-23 16:35:08 -0400

    Document process

--- src/main/java/com/perspecta/luegimport/business/domain/document_wrapper/DocumentWrapperRepository.java
+++ src/main/java/com/perspecta/luegimport/business/domain/document_wrapper/DocumentWrapperRepository.java
@@ -3,6 +3,9 @@ package com.perspecta.luegimport.business.domain.document_wrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DocumentWrapperRepository extends JpaRepository<DocumentWrapper, Long> {
+	List<DocumentWrapper> findByUserNameEquals(String userName);
 }
