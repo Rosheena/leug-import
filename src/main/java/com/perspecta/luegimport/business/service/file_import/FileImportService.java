8fc6cbb8e8ef1f76fc08818fa525b965f5f32857
rkhan <rkhan@wheelsup.com>
2019-07-22 23:57:45 -0400

    parsing file

--- src/main/java/com/perspecta/luegimport/business/service/file_import/FileImportService.java
+++ src/main/java/com/perspecta/luegimport/business/service/file_import/FileImportService.java
@@ -3,41 +3,28 @@ package com.perspecta.luegimport.business.service.file_import;
 import com.perspecta.luegimport.business.domain.document.Document;
 import com.perspecta.luegimport.business.domain.document_wrapper.DocumentWrapper;
 import com.perspecta.luegimport.business.domain.user.User;
+import com.perspecta.luegimport.business.service.file_import.delegate.FileImportDelegate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class FileImportService {
 
+	private final FileImportDelegate fileImportDelegate;
+
 	public List<Document> validate(String userName, MultipartFile file){
 		// parse file
-
-		List<DocumentWrapper> documentWrapperList = new ArrayList<>();
-
-		try {
-			InputStream inputFileStream = file.getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(inputFileStream));
-			documentWrapperList = br.lines().skip(1).map(line -> mapToDocumentWrapper(line, userName)).collect(Collectors.toList());
-			br.close();
-		} catch (Exception ex){
-			log.error("Error processing the file: "+ex);
-		}
+		List<DocumentWrapper> documentWrapperList = fileImportDelegate.parseFile(userName, file);
 
 		// validate file
-		//
+
 		return null;
 	}
 
@@ -48,47 +35,6 @@ public class FileImportService {
 
 	}
 
-	private DocumentWrapper mapToDocumentWrapper(String line, String userName){
-		String[] tokens = line.split(",");// a CSV has comma separated lines
-		DocumentWrapper documentWrapper = new DocumentWrapper();
-		Document document = new Document();
-		documentWrapper.setIsValidated(true);
-
-		if(StringUtils.isNotBlank(tokens[0])){
-			document.setObjectType(StringUtils.trimToEmpty(tokens[0]));
-		}
-
-		if(StringUtils.isNotBlank(tokens[1])){
-			document.setObjectName(StringUtils.trimToEmpty(tokens[1]));
-		} else {
-			documentWrapper.setIsValidated(false);
-		}
-
-		if(StringUtils.isNotBlank(tokens[2])){
-			document.setFileLocation(StringUtils.trimToEmpty(tokens[2]));
-		} else {
-			documentWrapper.setIsValidated(false);
-		}
-
-		if(StringUtils.isNotBlank(tokens[3])){
-			document.setCpId(StringUtils.trimToEmpty(tokens[3]));
-		} else {
-			documentWrapper.setIsValidated(false);
-		}
-
-		if(StringUtils.isNotBlank(tokens[4])){
-			document.setSubType(StringUtils.trimToEmpty(tokens[4]));
-		}
-
-		if(StringUtils.isNotBlank(tokens[5])){
-			document.setYear(StringUtils.trimToEmpty(tokens[5]));
-		}
-
-		documentWrapper.setDocument(document);
-		documentWrapper.setUserName(userName);
-
-		return documentWrapper;
-	}
 
 
 
--- src/main/java/com/perspecta/luegimport/business/service/file_import/delegate/FileImportDelegate.java
+++ src/main/java/com/perspecta/luegimport/business/service/file_import/delegate/FileImportDelegate.java
@@ -1,16 +1,81 @@
 package com.perspecta.luegimport.business.service.file_import.delegate;
 
 import com.perspecta.luegimport.business.domain.document.Document;
+import com.perspecta.luegimport.business.domain.document_wrapper.DocumentWrapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class FileImportDelegate {
 
-	/*public List<Document> parseFile(MultipartFile file){
+	public List<DocumentWrapper> parseFile(String userName, MultipartFile file){
 
-		Path filePath = file.get
+		List<DocumentWrapper> documentWrapperList = new ArrayList<>();
 
-	}*/
+		try {
+			InputStream inputFileStream = file.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputFileStream));
+			documentWrapperList = br.lines().skip(1).map(line -> mapToDocumentWrapper(line, userName)).collect(Collectors.toList());
+			br.close();
+		} catch (Exception ex){
+			log.error("Error processing the file: "+ex);
+		}
+
+		return documentWrapperList;
+	}
+
+	private DocumentWrapper mapToDocumentWrapper(String line, String userName){
+		String[] tokens = line.split(",");// a CSV has comma separated lines
+		DocumentWrapper documentWrapper = new DocumentWrapper();
+		Document document = new Document();
+		documentWrapper.setIsValidated(true);
+
+		if(StringUtils.isNotBlank(tokens[0])){
+			document.setObjectType(StringUtils.trimToEmpty(tokens[0]));
+		}
+
+		if(StringUtils.isNotBlank(tokens[1])){
+			document.setObjectName(StringUtils.trimToEmpty(tokens[1]));
+		} else {
+			documentWrapper.setIsValidated(false);
+		}
+
+		if(StringUtils.isNotBlank(tokens[2])){
+			document.setFileLocation(StringUtils.trimToEmpty(tokens[2]));
+		} else {
+			documentWrapper.setIsValidated(false);
+		}
+
+		if(StringUtils.isNotBlank(tokens[3])){
+			document.setCpId(StringUtils.trimToEmpty(tokens[3]));
+		} else {
+			documentWrapper.setIsValidated(false);
+		}
+
+		if(StringUtils.isNotBlank(tokens[4])){
+			document.setSubType(StringUtils.trimToEmpty(tokens[4]));
+		}
+
+		if(StringUtils.isNotBlank(tokens[5])){
+			document.setYear(StringUtils.trimToEmpty(tokens[5]));
+		}
+
+		documentWrapper.setDocument(document);
+		documentWrapper.setUserName(userName);
+
+		return documentWrapper;
+	}
+
 }
