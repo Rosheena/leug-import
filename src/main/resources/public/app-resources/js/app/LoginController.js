b97e5e408bc54564c24145b35e3a1e926af8f738
Rosheena <rosheena.khan@gmail.com>
2019-08-08 15:32:21 -0400

    Document process

--- src/main/resources/public/app-resources/js/app/LoginController.js
+++ src/main/resources/public/app-resources/js/app/LoginController.js
@@ -1,27 +1,43 @@
-LuegImportApp.controller('LoginController',
-    function ($scope, $rootScope, $q, $http, AuthenticationService){
+LuegImportApp.controller('LoginController', ['$rootScope', '$state', '$q', '$http', 'AuthenticationService', function ($rootScope, $state, $q, $http, AuthenticationService, RolesService) {
 
-        $scope.username = null;
-        $scope.password = null;
-        $scope.error = null;
+    let vm = this;
 
-        $scope.init = function (){
-            AuthenticationService.clearAuthorization();
-            $rootScope.current_user = null;
-        };
+    vm.username = null;
+    vm.password = null;
+    vm.error = null;
 
-        $scope.login = function () {
-            $scope.error = null;
+    vm.init = function () {
+        AuthenticationService.clearAuthorization();
+    };
 
-            AuthenticationService.authenticate($scope.username, $scope.password)
-                .then(
-                    function () {
-                        console.log("to home")
-                        $rootScope.navigateTo('/home')
-                    },
-                    function (err) {
-                        $scope.error = err;
+    vm.login = function () {
+        vm.error = null;
+
+        AuthenticationService.authenticate(vm.username, vm.password)
+            .then(
+                function (user) {
+
+                    switch (user.authority) {
+                        // these are the default states for each role when they login
+                        case RolesService.roles.gama :
+                            $state.go('gama.managePilots');
+                            break;
+                        case RolesService.roles.tmc :
+                            console.log("in tmc, going to tmc")
+                            $state.go('tmc.managePilots');
+                            break;
+                        case RolesService.roles.super :
+                            $state.go('gama.managePilots');
+                            break;
+                        default :
+                            $state.go('login');
                     }
-                )
-        }
-    });
+
+                },
+                function (err) {
+                    vm.error = err;
+                }
+            )
+    }
+}]);
+
