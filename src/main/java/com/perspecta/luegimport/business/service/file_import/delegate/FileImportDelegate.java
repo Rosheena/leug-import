ef9af28831a6c62dca6999e7d96c24c93cf9048a
Rosheena <rkhan@wheelsup.com>
2019-08-21 17:15:06 -0400

    new changes

--- src/main/java/com/perspecta/luegimport/business/service/file_import/delegate/FileImportDelegate.java
+++ src/main/java/com/perspecta/luegimport/business/service/file_import/delegate/FileImportDelegate.java
@@ -14,14 +14,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -38,22 +32,6 @@ public class FileImportDelegate {
 		this.documentRepository = documentRepository;
 		this.documentWrapperRepository = documentWrapperRepository;
 		this.modelMapper = modelMapper;
-	}
-
-	public List<DocumentWrapper> parseFile(String userName, MultipartFile file) {
-
-		List<DocumentWrapper> documentWrapperList = new ArrayList<>();
-
-		try {
-			InputStream inputFileStream = file.getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(inputFileStream));
-			documentWrapperList = br.lines().skip(1).map(line -> mapToDocumentWrapper(line, userName)).collect(Collectors.toList());
-			br.close();
-		} catch (Exception ex) {
-			log.error("Error processing the file: " + ex);
-		}
-
-		return documentWrapperList;
 	}
 
 	public void validateFields(List<DocumentWrapper> documentWrapperList) {
@@ -157,40 +135,6 @@ public class FileImportDelegate {
 		documentWrapperViewList.get(0).setDocumentErrorType(DocumentErrorType.INVALID_CPID);
 
 		return documentWrapperViewList;
-	}
-
-	private DocumentWrapper mapToDocumentWrapper(String line, String userName) {
-		String[] tokens = line.split(",");// a CSV has comma separated lines
-		DocumentWrapper documentWrapper = new DocumentWrapper();
-		Document document = new Document();
-
-		if (StringUtils.isNotBlank(tokens[0])) {
-			document.setObjectType(StringUtils.trimToEmpty(tokens[0]));
-		}
-
-		if (StringUtils.isNotBlank(tokens[1])) {
-			document.setObjectName(StringUtils.trimToEmpty(tokens[1]));
-		}
-
-		if (StringUtils.isNotBlank(tokens[2])) {
-			document.setFileLocation(StringUtils.trimToEmpty(tokens[2]));
-		}
-
-		if (StringUtils.isNotBlank(tokens[3])) {
-			document.setCpId(StringUtils.trimToEmpty(tokens[3]));
-		}
-
-		if (StringUtils.isNotBlank(tokens[4])) {
-			document.setSubType(StringUtils.trimToEmpty(tokens[4]));
-		}
-
-		if (StringUtils.isNotBlank(tokens[5])) {
-			document.setYear(StringUtils.trimToEmpty(tokens[5]));
-		}
-
-		documentWrapper.setDocument(document);
-
-		return documentWrapper;
 	}
 
 }
