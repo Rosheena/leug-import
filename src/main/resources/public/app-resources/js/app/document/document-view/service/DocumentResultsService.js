92a29b0c6ab0c3ebe637b4e5deffc383ae517f13
Rosheena <rkhan@wheelsup.com>
2019-08-26 12:37:18 -0400

    new changes

--- src/main/resources/public/app-resources/js/app/document/document-view/service/DocumentResultsService.js
+++ src/main/resources/public/app-resources/js/app/document/document-view/service/DocumentResultsService.js
@@ -4,7 +4,7 @@ LuegImportApp.service('DocumentResultsService', ['CrudHTTPService', 'Notificatio
     this.processedResults = [];
     this.userHasSearched = false;
 
-    this.getProcessedDocuments() = function (callback) {
+    this.getProcessedDocuments = function (callback) {
         let url = 'app/lueg/document/processed-results';
         CrudHTTPService.read(url, callback);
     }
